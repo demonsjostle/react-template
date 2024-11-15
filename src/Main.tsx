0e1ee9b6ca2d763376d6f86d087b6d87da10e29b
demonsjostle <demons.jostle@gmail.com>
2024-11-16 05:34:20 +0700

    update Welcome page

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./App.css";
 
-const Main = () => {
-  return <div>Main</div>;
+const Welcome: React.FC = () => {
+  return (
+    <div className="welcome-container">
+      <div className="welcome-content">
+        <h1 className="welcome-title">Welcome</h1>
+        <p className="welcome-description">Discover the art of simplicity</p>
+      </div>
+      <div className="background-gradient"></div>
+    </div>
+  );
 };
 
-export default Main;
+export default Welcome;
